2a0931b545536ac15b42d82e12ae67e368b5e45e
İkra Başer <ikkraa2004@gmail.com>
2026-07-28 11:52:12 +0000

    feat(rbac): enforce module access at request level

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,10 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "apps.accounts.middleware.ModuleAccessMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
 ]
 
 
